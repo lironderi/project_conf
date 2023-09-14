6586079c374b7eed48368e9860cc109efbe54193
liron deri <liron.deri3@outlook.com>
2023-09-14 19:02:32 +0300

    minikube test 10

--- eks_conf/provider.tf
+++ eks_conf/provider.tf
@@ -1,12 +1,20 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.5.1"
     }
   }
 }
