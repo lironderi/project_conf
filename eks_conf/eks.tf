8f9b799dba825f0a3ff6757a60809b4e242a857d
liron deri <liron.deri3@outlook.com>
2023-09-14 17:36:53 +0300

    minikube test 8

--- eks_conf/eks.tf
+++ eks_conf/eks.tf
@@ -50,6 +50,6 @@ resource "aws_eks_cluster" "market-app" {
 resource "aws_eks_addon" "addons" {
   cluster_name      = aws_eks_cluster.market-app.name
   addon_name        = "aws-ebs-csi-driver"
-  addon_version     = "v1.4.0-eksbuild.preview"
+  addon_version     = "v1.11.2-eksbuild.1"
   resolve_conflicts = "OVERWRITE"
 }
