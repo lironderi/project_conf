720681cb23182d8aaa3bd02abef53b2568b6a220
liron deri <liron.deri3@outlook.com>
2023-09-19 16:13:42 +0300

    order

--- eks_conf/provider.tf
+++ eks_conf/provider.tf
@@ -4,20 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "= 2.5.1"
-    }
-  }
 }
 provider "aws" {
   region = "us-east-1"
-}
-provider "helm" {
-  kubernetes {
-    config_path = pathexpand(var.kube_config)
-  }
-}
-provider "kubernetes" {
-  config_path = pathexpand(var.kube_config)
 }
