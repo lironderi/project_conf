76a18c03c220c2ccebee8e763221feae4356490c
liron deri <liron.deri3@outlook.com>
2023-09-26 17:06:44 +0300

    testing workflow 6

--- eks_conf/s3.tf
+++ eks_conf/s3.tf
@@ -1,9 +1,13 @@
+data "aws_s3_bucket" "existing" {
+  bucket = var.bucket_name
+}
 resource "aws_s3_bucket" "tf-state-bucket" {
+  count  = length(data.aws_s3_bucket.existing) == 0 ? 1 : 0
   bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_object" "object" {
-  count  = var.create_resource ? 1 : 0
+  count  = length(data.aws_s3_bucket.existing) == 0 ? 1 : 0
   key    = "terraform.tfstate"
   bucket = var.bucket_name
   source = var.state
