0821805701794f4b9de37f004cd76fec8bb3cb33
liron deri <liron.deri3@outlook.com>
2023-09-26 11:07:04 +0300

    sync

--- eks_conf/provider.tf
+++ eks_conf/provider.tf
@@ -4,8 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+
 }
-}
+} 
 provider "aws" {
   region = "us-east-1"
 }
