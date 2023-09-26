70fdf5f451cfd61856bd9c767c252849ffd4bc5c
daniel <daniel@daniel>
2023-09-26 09:08:14 +0300

    comment

--- eks_conf/s3.tf
+++ eks_conf/s3.tf
@@ -25,12 +25,12 @@ resource "aws_s3_bucket_acl" "example" {
   acl    = "private"
 }
 
-# resource "aws_s3_bucket_object" "object" {
-#   count  = var.create_resource ? 1 : 0
-#   key    = "terraform.tfstate"
-#   bucket = aws_s3_bucket.tf-state-bucket.id
-#   source = "./terraform.tfstate"
-# }
+resource "aws_s3_bucket_object" "object" {
+  count  = var.create_resource ? 1 : 0
+  key    = "terraform.tfstate"
+  bucket = aws_s3_bucket.tf-state-bucket.id
+  source = "./terraform.tfstate"
+}
 
 
 
