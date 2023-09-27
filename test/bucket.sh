3c8ab4cc31efeebad12d4678355883f78eea487f
liron deri <liron.deri3@outlook.com>
2023-09-27 16:23:53 +0300

    test bucket 12

---
+++ test/bucket.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+exist=$(aws s3 ls s3://market-app-bucket-prod-liron/ | grep counter.txt)
+if [ "$exist" -gt 0 ]; then
+touch counter.txt && echo "0" > counter.txt && aws cp counter.txt s3://market-app-bucket-prod-liron/
+else
+counter=aws s3 cp s3://market-app-bucket-prod-liron/counter.txt
+counter = counter+1
+echo $counter > counter.txt
+aws s3 sync ./counter.txt s3://market-app-bucket-prod-liron/counter.txt
+fi
+echo $counter
