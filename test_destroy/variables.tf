aa9a24c89b947ea9b00a5f69c50ce9cd6537d070
liron deri <liron.deri3@outlook.com>
2023-09-27 12:16:03 +0300

    test destroy 4

--- test_destroy/variables.tf
+++ test_destroy/variables.tf
@@ -6,4 +6,9 @@ variable "bucket_name" {
   type = string
   description = "bucket name"
 }
+variable "create_resource" {
+  type        = bool
+  default     = true
+  description = "controls whether the resource should be created"
+}
 #test 2
