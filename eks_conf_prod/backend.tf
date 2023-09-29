e27e1f3e67c5b452cd71313175698d989a9caa9c
liron deri <liron.deri3@outlook.com>
2023-09-29 11:14:38 +0300

    testing backend 29/09/23 2

--- eks_conf_prod/backend.tf
+++ eks_conf_prod/backend.tf
@@ -1,6 +1,8 @@
-backend "s3" {
+terraform {
+  backend "s3" {
     bucket = "market-app-bucket-prod-liron"
     key    = "terraform.tfstate"
     region = "us-east-1"
     profile = "default"
+  }
 }
