76b2ae0f43e33b9a9b8d5e99f0bfa5f6dd1859ac
liron deri <liron.deri3@outlook.com>
2023-10-02 20:14:46 +0300

    order

--- argo-cd_conf/backend.tf
+++ argo-cd_conf/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "market-app-bucket-liron"
-    key    = "terraform.tfstate"
+    key    = "argocd-terraform.tfstate"
     region = "us-east-1"
     profile = "default"
   }
 }
-
--- monitoring_conf/backend.tf
+++ monitoring_conf/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "market-app-bucket-liron"
-    key    = "terraform.tfstate"
+    key    = "monitoring-terraform.tfstate"
     region = "us-east-1"
     profile = "default"
   }
 }
-
