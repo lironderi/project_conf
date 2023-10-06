1252788afa5e20bf7a954472f7eec5dd4b7a162b
danielsagie <137056545+danielsagie@users.noreply.github.com>
2023-10-06 14:11:22 +0300

    Update backend.tf

--- monitoring_conf/backend.tf
+++ monitoring_conf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "market-app-bucket-daniell"
+    bucket = "market-app-bucket-danielll"
     key    = "monitoring-terraform.tfstate"
     region = "us-east-1"
     profile = "default"
