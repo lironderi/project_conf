ce19eeb5661b2dec7eef3ee4eac5a80f30d0be94
danielsagie <137056545+danielsagie@users.noreply.github.com>
2023-10-06 13:48:30 +0300

    Update backend.tf

--- argo-cd_conf/backend.tf
+++ argo-cd_conf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "market-app-bucket-liron"
+    bucket = "market-app-bucket-daniel"
     key    = "argocd-terraform.tfstate"
     region = "us-east-1"
     profile = "default"
