216f29319d592ddde1621d264fadcf848e693384
danielsagie <137056545+danielsagie@users.noreply.github.com>
2023-11-05 16:04:37 +0200

    Update provider.tf

--- argo-cd_conf/provider.tf
+++ argo-cd_conf/provider.tf
@@ -6,13 +6,4 @@ provider "helm" {
 provider "kubernetes" {
     config_path = pathexpand(var.kube_config)
 }
-terraform {
-  required_version = ">= 1.0.0"
 
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "= 2.5.1"
-    }
-  }
-}
